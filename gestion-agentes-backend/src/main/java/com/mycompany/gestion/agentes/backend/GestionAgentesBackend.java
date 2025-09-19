aeb825e420c833f44d9b5de3efab089c3a4fce1a
Jose <rivera991113@gmail.com>
2025-09-19 10:00:05 -0500

    chore: inicializar proyecto Java, README y .gitignore

---
+++ gestion-agentes-backend/src/main/java/com/mycompany/gestion/agentes/backend/GestionAgentesBackend.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestion.agentes.backend;
+
+/**
+ *
+ * @author Admin
+ */
+public class GestionAgentesBackend {
+    public static void main(String[] args) {
+        System.out.println("Gestión de Agentes - PMV: servicio inicial");
+ }
+    
+}
